9a4f4c609fac9edbbd9653e0b6cd99d348bdb478
JaakkoNurm <jaakko.nurminen02@gmail.com>
2025-07-01 20:31:37 +0300

    Added intial dockerfiles for front & backend

--- api/config.py
+++ api/config.py
@@ -1,10 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
 from datetime import timedelta
 
 class Config:
-  PG_HOST = "localhost"
-  PG_DATABASE = "postgres"
-  PG_USER = "postgres"
-  PG_PASSWORD = "postgres"
-  PG_PORT = 5432
-  JWT_SECRET = "b394c5abe1ec5fb08111c1ee"
-  JWT_TOKEN_EXPIRES = timedelta(hours=1)
+    PG_HOST = os.getenv("PG_HOST", "localhost")
+    PG_DATABASE = os.getenv("PG_DATABASE", "postgres")
+    PG_USER = os.getenv("PG_USER", "postgres")
+    PG_PASSWORD = os.getenv("PG_PASSWORD", "")
+    PG_PORT = int(os.getenv("PG_PORT", 5432))
+
+    JWT_SECRET = os.getenv("JWT_SECRET", "super-secret")
+    JWT_TOKEN_EXPIRES = timedelta(hours=int(os.getenv("JWT_EXPIRE_HOURS", 1)))
