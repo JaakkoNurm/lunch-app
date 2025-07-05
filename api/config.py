46d1960590e6c0985782f98e32168edf44d03796
JaakkoNurm <jaakko.nurminen02@gmail.com>
2025-07-05 18:14:44 +0300

    environment variables for dockerization

--- api/config.py
+++ api/config.py
@@ -1,11 +1,21 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 import os
 from datetime import timedelta
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Check if we're in development mode
+ENVIRONMENT = os.getenv("FLASK_ENV", "development")
+print("Flask server running in", ENVIRONMENT)
+
+if ENVIRONMENT == "development":
+    # Load .env only in development
+    load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / '.env')
 
 class Config:
-    PG_HOST = os.getenv("PG_HOST", "localhost")
+    if ENVIRONMENT == "development":
+        PG_HOST = "localhost"
+    else:
+        PG_HOST = os.getenv("PG_HOST", "localhost")
     PG_DATABASE = os.getenv("PG_DATABASE", "postgres")
     PG_USER = os.getenv("PG_USER", "postgres")
     PG_PASSWORD = os.getenv("PG_PASSWORD", "")
--- api/run-flask-dev.sh
+++ api/run-flask-dev.sh
@@ -2,4 +2,5 @@
 source ./venv/bin/activate
 pip install -r requirements.txt
 export FLASK_DEBUG=1
+export FLASK_ENV='development'
 python3 -m flask --app api/index run -p 5328
